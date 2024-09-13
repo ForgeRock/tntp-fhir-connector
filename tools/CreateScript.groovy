df2ae6b90f9f093d9ec78e75fccb0a98d522cf5d
TylerGelinas <94064355+TylerGelinas@users.noreply.github.com>
2024-09-12 23:33:59 -0500

    Update CreateScript.groovy
    Signed-off-by: TylerGelinas <94064355+TylerGelinas@users.noreply.github.com>

--- tools/CreateScript.groovy
+++ tools/CreateScript.groovy
@@ -18,13 +18,8 @@ import org.identityconnectors.framework.common.objects.Attribute
 import org.identityconnectors.framework.common.objects.AttributesAccessor
 import org.identityconnectors.framework.common.objects.ObjectClass
 import org.identityconnectors.framework.common.objects.OperationOptions
-import org.identityconnectors.framework.common.objects.*
-
-import org.forgerock.openicf.connectors.scriptedrest.ScriptedRESTUtils
 import java.util.Iterator
 import java.util.HashMap
-import static groovyx.net.http.Method.POST
-import static groovyx.net.http.Method.PUT
 
 def operation = operation as OperationType
 
@@ -40,7 +35,6 @@ def logPrefix = "[Epic] [CreateScript]: "
 log.error(logPrefix + "Entering " + operation + " Script");
 def createAttributes = new AttributesAccessor(attributes as Set<Attribute>)
 
-
 switch (objectClass) {
     case ObjectClass.ACCOUNT:
         log.error(logPrefix + "Entering ObjectClass.ACCOUNT case in " + operation + " Script");
@@ -49,86 +43,26 @@ switch (objectClass) {
 
         for(Iterator i = attributes.iterator();i.hasNext();){
             Attribute thisAt = i.next();
+            log.error(logPrefix + "Here is thisAt name: " + thisAt.getName() + " and here is thisAts value: " + thisAt.getValue());
             hm.put(thisAt.getName(), thisAt.getValue());
         }
         def builder = new JsonBuilder()
         def dob = hm.get("dateOfBirth");
-        def sn = hm.get("sn");
-        def givenName = hm.get("givenName");
-        def telephoneNumber = hm.get("telephoneNumber");
-        def postalAddress = hm.get("postalAddress");
-        def city = hm.get("city");
-        def state = hm.get("stateProvince");
-        def postalCode = hm.get("postalCode");
-        def country = hm.get("country");
-        def description = hm.get("description");
-
         dob = dob.get(0)
-        log.error(dob)
-        sn = sn.get(0)
-        givenName = givenName.get(0)
-        telephoneNumber = telephoneNumber.get(0)
-        postalAddress = postalAddress.get(0)
-        city = city.get(0)
-        state = state.get(0)
-        postalCode = postalCode.get(0)
-        country = country.get(0)
-
-        def jsonString = "{\n" +
-                "  \"resourceType\": \"Patient\",\n" +
-                "  \"identifier\": [\n" +
-                "    {\n" +
-                "      \"use\": \"usual\",\n" +
-                "      \"system\": \"urn:oid:2.16.840.1.113883.4.1\",\n" +
-                "      \"value\": \"${description}\"\n" +
-                "    }\n" +
-                "  ],\n" +
-                "  \"gender\": \"male\",\n" +
-                "  \"name\": [\n" +
-                "    {\n" +
-                "      \"use\": \"usual\",\n" +
-                "      \"family\": \"${sn}\",\n" +
-                "      \"given\": [\n" +
-                "        \"${givenName}\"\n" +
-                "      ]\n" +
-                "    }\n" +
-                "  ],\n" +
-                "  \"birthDate\": \"${dob}\"\n" +
-                "  \n" +
-                "}"
-
+        log.error("JSON STRING")
+        def jsonString = "{ \"resourceType\": \"Patient\", \"birthDate\": \"${dob}\"}"
         println jsonString
+        def response = connection.post(
+                                  path: '/fhir/Patient',
+                                  contentType: JSON,
+                                  requestContentType: JSON,
+                                  body: jsonString);
 
-        // connection.request(POST, JSON) { req ->
-        //     uri.path = "/interconnect-fhir-oauth/oauth2/token/"
-        //     headers.'Content-Type' = 'application/x-www-form-urlencoded'
-        //     body = jsonString
-
-        //     response.success = { resp, json ->
-        //         log.error(resp.status.toString())
-        //         log.error(resp)
-        //         return local
-        //     }
-
-        // }
-
-        return connection.request(POST, JSON) { req ->
-            uri.path = "/interconnect-fhir-oauth/api/FHIR/R4/Patient/"
-            headers.'Authorization' = "Bearer "
-            body = jsonString
-
-            response.success = { resp, json ->
-                log.error(resp.status.toString())
-                location = resp.headers['location'].toString()
-                local = location.substring(location.lastIndexOf("/") + 1)
-                log.error(local)
-                return local
-            }
-
-        }
-
-
-
+        log.error(logPrefix + "sent post.  Here is the response status : " + response.getStatus());
+        log.error(logPrefix + "sent post.  Here is the response data : " + response.getData());
+        log.error(logPrefix + "sent post.  Here is the response patient ID : " + response.getData().id);
+       name = response.getData().id
+        log.error("name" + name)
         break
 }
 return name
